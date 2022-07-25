458e32d6682ede22f743d59aacaa810f4ec54ac9
joilsonlira <joilson.lira@live.com>
2022-07-25 11:26:45 -0300

    UPDATE: trabalhando na responsividade

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -15,3 +15,17 @@ function close_modal(modal_name){
 
         modal.style.display = 'none';
 }
+
+
+//HAMBURGER MENU
+var hamburger = document.querySelector(".hamburger");
+var nav_menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav_menu.classList.toggle("active");
+})
+document.querySelectorAll(".nav_link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav_menu.classList.remove("active");
+}))
